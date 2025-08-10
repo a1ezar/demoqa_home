c1432f5b630de465e550fefa825aef2e918fdef3
a1ezar <chubarovd2004@gmail.com>
2025-08-10 23:48:06 +0300

    Fix logic

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,9 +1,20 @@
 from pages.swag_labs import SwagLabs
-import time
+
 
 def test_check_icon(browser):
     swag_lab_page = SwagLabs(browser)
     swag_lab_page.visit()
     assert swag_lab_page.exist_icon()
+
+
+def test_check_username(browser):
+    swag_lab_page = SwagLabs(browser)
+    swag_lab_page.visit()
     assert swag_lab_page.exist_username_field()
+    
+    
+def test_check_password(browser):
+    swag_lab_page = SwagLabs(browser)
+    swag_lab_page.visit()
     assert swag_lab_page.exist_password_field()
+
